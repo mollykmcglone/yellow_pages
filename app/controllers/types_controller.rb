81f22c261d1bbd1467fc24ba1814018bf9033df1
Bang Nguyen and Molly McGlone <bang+molly@>
2016-09-06 15:56:38 -0700

    add edit and update type route and view

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -23,6 +23,20 @@ class TypesController < ApplicationController
     end
   end
 
+  def edit
+    @type = Type.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @type = Type.find(params[:id])
+    if @type.update(type_params)
+      redirect_to types_path
+    else
+      render :edit
+    end
+  end
+
   private
   def type_params
     params.require(:type).permit(:name)
