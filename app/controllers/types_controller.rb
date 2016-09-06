caf18e166e1ab823e23b78e8b98b752a62034519
Bang Nguyen and Molly McGlone <bang+molly@>
2016-09-06 15:52:00 -0700

    add new type route and view

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -8,4 +8,23 @@ class TypesController < ApplicationController
     @type = Type.find(params[:id])
     render :show
   end
+
+  def new
+    @type = Type.new
+    render :new
+  end
+
+  def create
+    @type = Type.new(type_params)
+    if @type.save
+      redirect_to types_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def type_params
+    params.require(:type).permit(:name)
+  end
 end
